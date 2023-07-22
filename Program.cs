ab58c0a9eb59d36f02042b91c9755bef1fb0ebfb
Natalliax <123833294+Natalliax@users.noreply.github.com>
2023-07-22 23:20:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using MySqlConnector;
+using System;
+using System.Threading.Tasks;
+
+
+namespace DB_MySQL
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var builder = new MySqlConnectionStringBuilder("Server = 91.149.187.115; port = 43251; UserID = m_n_p; Password = Ejik_7534392; Database = chik_db")
+            {
+                
+            };
+
+            using var connection = new MySqlConnection(builder.ConnectionString);
+            await connection.OpenAsync();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = "SELECT * from nameservice";
+
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var value = reader.GetValue(1);
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
+
+        
